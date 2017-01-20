fa430c278f6b9ab5da88c18e611a7ff5a007e3de
Pete Sevander <pete@sevander.com>
2017-01-20 20:37:27 +0200

    some initial initialization

---
+++ src/main.js
@@ -0,0 +1,20 @@
+
+window.onload = function() {
+
+    var game = new Phaser.Game(800, 800, Phaser.AUTO, '', { preload: preload, create: create });
+
+    function preload () {
+
+        game.load.image('logo', 'phaser.png');
+
+    }
+
+    function create () {
+
+        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+        logo.anchor.setTo(0.5, 0.5);
+
+    }
+
+};
+
