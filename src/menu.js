7f2339ebf1a5e54396874f09ab9fea6e342ffc84
Pete Sevander <pete@sevander.com>
2017-01-20 21:48:52 +0200

    fullscreen testing stuff

--- src/menu.js
+++ src/menu.js
@@ -1,10 +1,12 @@
 
 var menuState = {
     create: function() {
+        game.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
         var texti = game.add.button(80, 80, 'butan', this.startGame, this);
     },
 
     startGame: function() {
         game.state.start('play');
+        game.scale.startFullScreen(false);
     }
 };
