0f3711acc99b9458f485aca33f63dc7b505054bd
Juho Myllylahti <juho.myllylahti@iki.fi>
2017-01-20 23:34:09 +0200

    Initial audio functionality

--- src/js/boot.js
+++ src/js/boot.js
@@ -3,6 +3,7 @@
 var bootState = {
     preload: function() {
         game.load.image('logo', './assets/phaser.png');
+        game.load.audio('testmusic', ['./assets/sound/Keinzweiter_Mircoobee.mp3']);
     },
     create: function() {
         game.state.start('menu');
