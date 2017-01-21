23aaa32784fec991bb24ec7675d0fe762cdd6e12
Pete Sevander <pete@sevander.com>
2017-01-21 23:03:48 +0200

    intro stuff

--- src/js/intro.js
+++ src/js/intro.js
@@ -3,29 +3,23 @@ var introState = {
         this.music = game.add.audio('intromusic');
         this.music.loopFull();
 
-        this.bling = new Phaser.Rectangle(0, 0,
-                                          game.world.width,
-                                          game.world.height);
-
-
         game.add.text(0, 0, "introtexti", {font:"12px Arial", fill:"#eee"});
+
+        this.g = game.add.graphics(0, 0);
+        this.g.beginFill('#FFFFFF');
+        this.g.drawRect(0,0, game.world.width, game.world.height);
+        this.g.alpha = 0;
 
         this.game.input.onDown.addOnce(this.startGame, this);
     },
 
-    blingWhite: function() {
-
-    },
-
-    blingBlack: function() {
-
-    },
-
     startGame: function() {
         this.music.stop();
+        game.add.audio('startBling').play();
         // play bling
         game.scale.startFullScreen(false);
-        this.reallyStart();
+
+        fadeObj(this.g, 1, this.reallyStart);
     },
 
     reallyStart: function() {
