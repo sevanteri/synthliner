6f1298017ebe7523b2e28e87025f903c36af25b7
Joonas Reinikka <joonas.reinikka@gmail.com>
2017-01-21 20:03:39 +0200

    Fix mobile view

--- src/js/boot.js
+++ src/js/boot.js
@@ -20,7 +20,7 @@ var bootState = {
         game.scale.forceOrientation(true, false); // force portrait
         game.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
         game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
-        game.scale.setUserScale(2,2);
+        game.scale.setUserScale(1.5,1.5);
         game.state.start('intro');
     }
 };
