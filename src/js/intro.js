7d3a2411193ba2555aef6eb8499b2bc2177f2eba
Pete Sevander <pete@sevander.com>
2017-01-21 23:17:45 +0200

    intro text in game

--- src/js/intro.js
+++ src/js/intro.js
@@ -3,7 +3,40 @@ var introState = {
         this.music = game.add.audio('intromusic');
         this.music.loopFull();
 
-        game.add.text(0, 0, "introtexti", {font:"12px Arial", fill:"#eee"});
+        this.introText = game.add.text(game.world.centerX,
+                game.world.height,
+                "YEAR 20XX\n" +
+                "...\n" +
+                "THE INFAMOUS PROTON RIDER,\n" +
+                "CODENAME \"FAST TEDDY\", HAS\n" +
+                "ESCAPED FROM THE CELLARS OF\n" +
+                "THE UNDERWORLD.\n" +
+                "\n" +
+                "RIDING HIS PROTON BIKE\n" +
+                "FABULOUSLY TO THE NEON\n" +
+                "COLORED SUNSET, HE\n" +
+                "THREATENS TO TEAR APART\n" +
+                "THE VERY FABRIC OF TIME\n" +
+                "AND SPACE WITH THE\n" +
+                "GRAVITATIONAL WAVES.\n" +
+                "...\n" +
+                "YOU ARE THE SYNTH LINER,\n" +
+                "AN ABSTRACT ENTITY\n" +
+                "CREATED TO TRACE THE\n" +
+                "PROTON RIDER'S SUB-PARTICLE\n" +
+                "FUMES TO CANCEL THEIR\n" +
+                "EFFECT AND TO SAVE THE\n" +
+                "GALAXY.\n" +
+                "...\n" +
+                "WHAT ARE YOU WAITING\n" +
+                "FOR? GO GET HIM!",
+                {font:"12px bold Arial", fill:"#eee"});
+        this.introText.anchor.setTo(0.5, 0);
+
+        this.tw = game.add.tween(this.introText);
+        this.tw.to({y: -1000}, 30000, Phaser.Easing.Linear.None);
+        this.tw.onComplete.add(this.startGame, this);
+        this.tw.start();
 
         this.g = game.add.graphics(0, 0);
         this.g.beginFill('#FFFFFF');
