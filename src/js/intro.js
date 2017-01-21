03f11a834de3020bc7d727e4839dd1ab3eebc52e
Pete Sevander <pete@sevander.com>
2017-01-21 13:39:26 +0200

    some simple intro animations

--- src/js/intro.js
+++ src/js/intro.js
@@ -4,8 +4,19 @@ var introState = {
                                    90,
                                    'title');
         title.anchor.setTo(0.5, 0.5);
+        title.scale = {x:0, y:0};
 
-        var startbutton = game.add.button(20, 160, 'playButton', this.startGame, this);
+        tw = game.add.tween(title.scale);
+        tw.to({x: 1, y: 1}, 1000, Phaser.Easing.Linear.None);
+        tw.start();
+
+        var startbutton = game.add.button(this.game.world.centerX, 200, 'playButton', this.startGame, this);
+        startbutton.anchor.setTo(0.5, 0.5);
+
+        startbutton.scale = {x:0, y:0};
+        tw2 = game.add.tween(startbutton.scale);
+        tw2.to({x: 1, y: 1}, 1000, Phaser.Easing.Linear.None);
+        tw2.start();
     },
 
     startGame: function() {
